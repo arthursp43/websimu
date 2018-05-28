143cc0e0c8866aae8dc3981716a9beecf5c61c19
Arthur Silva Pereira <arthur.pereira@unimedvr.com.br>
2018-05-28 15:26:28 -0300

    instalando bootstrap

--- src/UsuarioBundle/Controller/DefaultController.php
+++ src/UsuarioBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('UsuarioBundle:Default:index.html.twig');
+        return $this->render('@Usuario/Default/index.html.twig');
     }
 }
