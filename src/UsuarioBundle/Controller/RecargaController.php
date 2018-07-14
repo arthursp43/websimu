514626d5880e0f1b77512ac1dfb4bd99533efb35
arthursp43 <arthursp43@gmail.com>
2018-07-14 12:51:39 -0300

    eae

--- src/UsuarioBundle/Controller/RecargaController.php
+++ src/UsuarioBundle/Controller/RecargaController.php
@@ -18,21 +18,78 @@ use UsuarioBundle\Repository\UsuarioRepository;
 class RecargaController extends Controller
 {
 
-    public function manterUsuario($login,$senha)
-    {
 
-    }
+
+
+
+
+
 
     /**
      * @Route("/recarga/inicio", name="recarga_inicio")
      */
     public function recargaInicioAction()
     {
-        var_dump("RECARGA SERÁ FEITA AQUI");exit;
+
+        //$request;
+        if(isset($_POST['login'])) {
+            $login = $_POST['login'];
+        }
+        else{
+            $login = $_COOKIE['login'];
+        }
+        if(isset($_POST['senha'])) {
+            $senha = $_POST['senha'];
+        }
+        else{
+            $senha = $_COOKIE['senha'];
+        }
+
+        /* @var UsuarioRepository
+         */
+        $UsuarioRepository = $this->getDoctrine()->getRepository('UsuarioBundle:Usuario');
+
+        /* @var UsuarioRepository
+         */
+        $LoginRepositorio = $this->getDoctrine()->getRepository('UsuarioBundle:Login');
+
+        /* @var CartaoRepository
+         */
+        $CartaoRepository = $this->getDoctrine()->getRepository('UsuarioBundle:Cartao');
+
+
+
+        $Login = $LoginRepositorio->buscaLogin($login,$senha);
+
+        //var_dump($log);exit;
+
+
+        if(count($Login)!=0)
+        {
+            //$usuario = "a";
+
+            $usuario = $UsuarioRepository->buscaUsuario($Login);
+
+
+            setcookie('login', $login);
+            setcookie('senha', $senha);
+            setcookie('usuario', $usuario->getIdusuario()."");
+
+            return $this->render('@Usuario/Recarga/homepage.html.twig',array(
+                'usuario'=>$usuario
+
+            ));
+        }
+        else
+        {
+
+
+            return $this->redirectToRoute('login', array('warning' => 1));
+            //return $this->render('@Usuario/Default/login.html.twig',array('warning'=>$warning));
+        }
+
 
     }
-
-
 
     
 
